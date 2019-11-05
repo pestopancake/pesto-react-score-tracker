6b065e9aa692df206af94c6df498ec0237ae5fe3
pestopancake <p@ulmart.in>
2019-11-05 01:13:37 +0000

    2 files modified

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ import { render } from 'react-dom';
 import Square from './Square';
 import Board from './Board';
 import './style.css';
+import NoSleep from 'nosleep.js';
 
+var noSleep = new NoSleep();
+
+function enableNoSleep() {
+  noSleep.enable();
+  document.removeEventListener('touchstart', enableNoSleep, false);
+}
+document.addEventListener('touchstart', enableNoSleep, false);
 
 class App extends Component {
   constructor() {
